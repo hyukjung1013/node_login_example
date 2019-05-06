21a1d5aa1f4c9fb29d90bcfa740fabac7fbd78f8
hyukjung <hyukjung1013@gmail.com>
2019-05-06 21:44:10 +0900

    6th Commit: (1) 'lowdb' added. (2) 'Sign Up', 'Log Out' added.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('../lib/passport');
 const router = express.Router();
+const db = require('../lib/lowdb');
 
 router.get('/login', (req, res) => {
     res.render('login');
@@ -13,15 +14,29 @@ router.post('/login_process',
     })
 );
 
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.post('/signup_process', (req, res) => {
+    var body = req.body;
+    var user = {
+        email: body.email,
+        password: body.password,
+        nickname: body.nickname
+    };
+    db.get('users').push(user).write();
+    req.login(user, function(err) {
+        // Automatically logged in after signed up.
+        return res.redirect('/');
+    });    
+});
+
 router.get('/logout', (req, res) => {
     req.logout();
     req.session.save(function() {
         res.redirect('/');
     });
-});
-
-router.get('/signup', (req, res) => {
-    res.render('signup');
 });
 
 module.exports = router;
